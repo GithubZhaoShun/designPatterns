4f1f9e9f215ab636bef31fcf2258ebcec44955b4
Jason <15022608992@163.com>
2019-07-29 16:38:45 +0800

    修改了Client注释

--- factory-method/src/designpatterns/factorymethod/Cilent.java
+++ factory-method/src/designpatterns/factorymethod/Cilent.java
@@ -7,6 +7,7 @@ package designpatterns.factorymethod;
  * @author Jason
  * @category 工厂方法模式简单示例
  * 工厂方法模式,要求产品类(HairTV,XiaomiTV)必须实现相同接口(TV)
+ * 工厂接口也可以是抽象类
  * 具体工厂实现工厂接口
  * 具体工厂全限定类名配置在config.xml中,无需修改代码,只需修改配置文件及可修改所需创建的对象,实现解耦
  */
